d67b09d76ba666ae6089797c08b48027ee2fcaf1
Filip Rudy <filip.rudy@sap.com>
2025-12-29 09:21:23 +0100

    add suggestions from code review

--- internal/cmd/dashboard/dashboard.go
+++ internal/cmd/dashboard/dashboard.go
@@ -27,9 +27,9 @@ func NewDashboardCMD(kymaConfig *cmdcommon.KymaConfig) *cobra.Command {
 			clierror.Check(runDashboard(&cfg))
 		}}
 
-	cmd.Flags().StringVarP(&cfg.port, "port", "p", "3001", `Specify the port on which the local dashboard will be exposed.`)
-	cmd.Flags().StringVar(&cfg.containerName, "container-name", "kyma-dashboard", `Specify the name of the local container.`)
-	cmd.Flags().BoolVarP(&cfg.verbose, "verbose", "v", true, `Enable verbose output with detailed logs.`)
+	cmd.Flags().StringVarP(&cfg.port, "port", "p", "3001", `Specifies the port on which the local dashboard will be exposed.`)
+	cmd.Flags().StringVar(&cfg.containerName, "container-name", "kyma-dashboard", `Specifies the name of the local container.`)
+	cmd.Flags().BoolVarP(&cfg.verbose, "verbose", "v", true, `Enables verbose output with detailed logs.`)
 
 	cmd.AddCommand(NewDashboardStartCMD(kymaConfig))
 	cmd.AddCommand(NewDashboardStopCMD(kymaConfig))
